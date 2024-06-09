8d80e8e91a98cf314b3469419ed681cc5d59c5ff
coyotepunk05 <coyote582005@gmail.com>
2024-06-08 20:07:07 -0700

    space failure fix

--- ENGR_103_FINAL.py
+++ ENGR_103_FINAL.py
@@ -1,4 +1,4 @@
-# import dictionary, common password
+# import common password list
 import random
 import string
 
@@ -7,7 +7,7 @@ Spec_ASCII = []
 
 for l in [range(33,48) , range(58,65) , range(91,97), range(123,127)]:
     for n in l:
-        Spec_ASCII.append(n)
+        Spec_ASCII.append(chr(n))
 
 # make list for fault analysis
 fault = {"number":0,"upper":0,"lower":0}
@@ -17,12 +17,15 @@ numberFaultIndices = []
 upperFaultIndices = []
 lowerFaultIndices = []
 
+# Init rating strength boundaries
+req = {5:"Very Weak",12:"Weak",15:"Moderate",25:"Strong",99999:"Very Strong"}
+
 # define commands
 def generate(pw=""):
 
     # Generate Random Password If none provided
     if pw == "":
-        return "".join(random.choices(string.ascii_letters + string.digits + string.punctuation, k = random.randint(19,30)))
+        return "".join(random.choices(string.ascii_letters + string.digits + "".join(Spec_ASCII), k = random.randint(19,30)))
     
     newstring = list(pw)
     #fix consecutive numbers
@@ -32,16 +35,45 @@ def generate(pw=""):
             # Excl Endpoints of each Block
             for i in range(tup[0]+1,tup[1]-1):
                 if random.choice([True,False]):
-                    newstring[i] = random.choice(string.ascii_letters + string.punctuation)
+                    newstring[i] = random.choice(string.ascii_letters + "".join(Spec_ASCII))
 
     #fix consecutive upper letters
     if fault["upper"] == 1:
-        pass
+            
+        for tup in upperFaultIndices:
+            # Excl Endpoints of each Block
+            for i in range(tup[0]+1,tup[1]-1):
+
+                # Randomly choice to replace an upper char with (number or spec. char) or (replace it with its lowercase variant)
+                if random.choice([True,False]):
+                    newstring[i] = random.choice(string.digits + "".join(Spec_ASCII))
+                elif random.choice([True,False]):
+                    newstring[i] = newstring[i].lower()
         
     #fix consecutive lower letters
     if fault["lower"] == 1:
-        pass
+                
+        for tup in lowerFaultIndices:
+            # Excl Endpoints of each Block
+            for i in range(tup[0]+1,tup[1]-1):
+            
+                # Randomly choice to replace an upper char with (number or spec. char) or (replace it with its uppercase variant)
+                if random.choice([True,False]):
+                    newstring[i] = random.choice(string.digits + "".join(Spec_ASCII))
+                elif random.choice([True,False]):
+                    newstring[i] = newstring[i].upper()
+
+
     #fix length
+    # min_passwordlen is minimum password length that is recommended
+    min_password_len = 7
+
+    if len(newstring) <= min_password_len:
+
+        r = random.randint(15,21)
+        
+        while len(newstring) <= r:
+            newstring.append(random.choice(string.ascii_letters + string.digits + "".join(Spec_ASCII)))
     
     return "".join(newstring)
 
@@ -79,23 +111,23 @@ def analyze(string):
     
     rating = 0
 
-    # Counts # of Characters (Assuming 1 pt per)
-    rating += len(string)
+    # Counts # of Characters (Assuming 0.5 pt per)
+    rating += 0.5 * len(string)
 
     # Count # of Numbers (Assuming 1 pt per)
     for c in string:
         if ord(c) < 58 and ord(c) > 47:
             rating += 1
     
-    # Count Spec. Char (Assuming 1 pt per)
+    # Count Spec. Char (Assuming 3 pt per)
     for c in string:
-        if ord(c) in Spec_ASCII:
-            rating += 1
+        if c in Spec_ASCII:
+            rating += 3
 
-    # Count Upper cases (Assuming 1 pt per)
+    # Count Upper cases (Assuming 1.5 pt per)
     for c in string:
         if ord(c) in range(65,91):
-            rating += 1
+            rating += 1.5
     
     # Count Little Letters (Assuming 1 pt per)
     for c in string:
@@ -119,7 +151,9 @@ def analyze(string):
                     if r == len(string):
                         break
                 if r - l >= minL:
-                    subtract -= r - l
+
+                    # Flat 7 pt deduction
+                    subtract = -7
                     L.append((l,r))
                 l = r
 
@@ -128,18 +162,19 @@ def analyze(string):
     # Count Consecutive Numbers
     global numberFaultIndices
     tmp, numberFaultIndices = substringConsec(47,58,4)
-    print(numberFaultIndices)
     if tmp < 0:
         fault["number"] = 1
     rating += tmp
 
     # Count Consecutive Upper Letters
+    global upperFaultIndices
     tmp, upperFaultIndices = substringConsec(65,91,7)
     if tmp < 0:
         fault["upper"] = 1
     rating += tmp
 
     # Count Consecutive Lower Letters
+    global lowerFaultIndices
     tmp, lowerFaultIndices = substringConsec(97,123,7)
     if tmp < 0:
         fault["lower"] = 1
@@ -150,13 +185,30 @@ def analyze(string):
 
 # take password input
 while True:
-
-    password = input("Input Password: ")
+    try:
+        password = input("Input Password: ")
+        for letter in password:
+            if letter == " ":
+                print("Do not enter spaces!")
+                space = True
+        if space:
+            continue
+    except:
+        pass
 
     # password aquisition
 
     if password == "":
-        print(generate())
+        emptystr = generate()
+        print(f'New Password: {emptystr}')
+        score = analyze(emptystr)
+        print(f'New Strength: {score}')
+
+        for number in req:
+            if score <= number:
+                print(f'Password Rating: {req[number]}')
+                strength = req[number]
+                break
         quit()
 
     if not all(ord(c) < 128 for c in password):
@@ -174,8 +226,6 @@ print(f'Password Strength: {score}')
 
 # tell user password strength and prompt for generation
 
-req = {5:"Very Weak",12:"Weak",15:"Moderate",25:"Strong",99999:"Very Strong"}
-
 for number in req:
     if score <= number:
         print(f'Password Rating: {req[number]}')
@@ -186,11 +236,13 @@ if strength == "Very Weak" or strength == "Weak" or strength == "Moderate":
     while True:
         yn = input("Would you like to automatically increase your password's strength? (yes/no): ").lower()
         if yn == "yes":
-            print(generate(password))
+            newpw = generate(password)
+            print("##########################################")
+            print(f'New Password: {newpw}')
+            print(f'New Strength: {analyze(newpw)}')
             break
         elif yn == "no":
             break
         else:
             print("Enter 'yes' or 'no'.")
             continue
-
